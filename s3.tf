e457674d457c2a553319a9a3076128c6933c9df0
Your Name <vemasani.pavani@gmail.com>
2026-02-25 09:43:08 +0530

    removed duplicate backend

--- s3.tf
+++ s3.tf
@@ -3,16 +3,8 @@ terraform {
     bucket         = "bucketinfo23"
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "statefile"
+
   }
 }
 
 
-resource "aws_dynamodb_table" "example"{
-  hash_key = "dbfile"
-  name     = "statefile"
-  attribute {
-    name = "dbfile"
-    type = "S" // here s is a string
-  }
-}

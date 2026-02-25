ba8e3f751fb5eca45da3cbd1271d72082c287c7a
Your Name <vemasani.pavani@gmail.com>
2026-02-25 10:23:15 +0530

    loop all listen ports

--- multiports/server.js
+++ multiports/server.js
@@ -1,15 +1,11 @@
 const http = require('http');
-
-// Create the server once
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(`Hello from Node.js! You hit ${req.socket.localPort}\n`);
-});
-
-// List of ports to listen on
 const ports = [3000, 4000];
-
 ports.forEach(port => {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(`Hello from Node.js! You hit port ${port}\n`);
+  });
+
   server.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
   });

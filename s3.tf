3e0e3901f76f0f5f4ce2b58b2ac8176eb4f72ccf
Your Name <vemasani.pavani@gmail.com>
2026-02-26 13:18:00 +0530

    backend path changed in s3

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "bucketinfo23"
-    key            = "state/terraform.tfstate"
+    key            = "state/dev/terraform.tfstate"
     region         = "us-east-1"
 
   }

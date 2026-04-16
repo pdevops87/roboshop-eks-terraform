5acfb2bd17844d571db8be6025e04fadb394734a
Your Name <vemasani.pavani@gmail.com>
2026-04-16 18:59:10 +0530

    create a helm argocd through terraform

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "argocd" {
   depends_on = [null_resource.kube-config]
   name       = "helm-chart"
-  repository = "helm repo add argo https://argoproj.github.io/argo-helm"
+  repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   set = [
     {

81aacb4a1873409f7a97f91ce07e98e3274f536a
Your Name <vemasani.pavani@gmail.com>
2026-05-15 07:47:19 +0530

    check aws login

--- modules/eks/prometheus.tf
+++ modules/eks/prometheus.tf
@@ -1,13 +1,14 @@
-# install prometheus
-resource "helm_release" "prometheus" {
-  depends_on = [null_resource.kube-config]
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  set {
-    name  = "prometheus.service.type"
-    value = "LoadBalancer"
-  }
-}
+# # install prometheus
+# resource "helm_release" "prometheus" {
+#   depends_on = [null_resource.kube-config]
+#   name       = "prometheus"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#
+#   set {
+#     name  = "prometheus.service.type"
+#     value = "LoadBalancer"
+#   }
+# }
 
 

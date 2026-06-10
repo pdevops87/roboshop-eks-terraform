f92b25add7bd4ffebe16e0e246c58382857f43f5
Your Name <vemasani.pavani@gmail.com>
2026-06-10 07:59:10 +0530

    port added explicitly

--- modules/eks/ingress.tf
+++ modules/eks/ingress.tf
@@ -7,8 +7,19 @@ resource "helm_release" "nginx-ingress" {
     {
       name = "controller.metrics.enabled"
       value = true
+    },
+    {
+      name = "controller.podAnnotations.prometheus\\.io/port"
+      value = 10254
+    },
+    {
+      name = "controller.podAnnotations.prometheus\\.io/scrape"
+      value = true
     }
   ]
 }
 
+
+# * controller.podAnnotations.prometheus\\.io/port=10254
+# * controller.podAnnotations.prometheus\\.op/scrape=true
 
